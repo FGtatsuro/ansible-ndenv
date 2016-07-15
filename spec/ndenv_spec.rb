f5c28926900b9b34cd113fd27476eb45caa94a35
FGtatsuro <fujiistorage@gmail.com>
2016-07-15 15:34:28 +0900

    Rafactor condition for spec testing.

--- spec/ndenv_spec.rb
+++ spec/ndenv_spec.rb
@@ -1,33 +1,36 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
-describe command("su -s /bin/bash -c 'ndenv versions' - #{ENV['ANYENV_OWNER']}"), :if => ['debian', 'ubuntu'].include?(os[:family]) do
-  its(:stdout) { should contain('v0.10.40') }
-  its(:stdout) { should contain('v0.12.4') }
-end
+if ENV['TARGET_HOST'].include?('container') then
+  describe command("su -s /bin/bash -c 'ndenv versions' - #{ENV['ANYENV_OWNER']}") do
+    its(:stdout) { should contain('v0.10.40') }
+    its(:stdout) { should contain('v0.12.4') }
+  end
 
-describe command("su -s /bin/bash -c 'ndenv global' - #{ENV['ANYENV_OWNER']}"), :if => ['debian', 'ubuntu'].include?(os[:family])  do
-  its(:stdout) { should contain('v0.10.40') }
-  its(:stdout) { should_not contain('v0.12.4') }
-end
+  describe command("su -s /bin/bash -c 'ndenv global' - #{ENV['ANYENV_OWNER']}") do
+    its(:stdout) { should contain('v0.10.40') }
+    its(:stdout) { should_not contain('v0.12.4') }
+  end
 
-describe command("su -s /bin/bash -c 'node --version' - #{ENV['ANYENV_OWNER']}"), :if => ['debian', 'ubuntu'].include?(os[:family]) do
-  its(:stdout) { should contain('v0.10.40') }
-  its(:stdout) { should_not contain('v0.12.4') }
-end
+  describe command("su -s /bin/bash -c 'node --version' - #{ENV['ANYENV_OWNER']}") do
+    its(:stdout) { should contain('v0.10.40') }
+    its(:stdout) { should_not contain('v0.12.4') }
+  end
 
-describe command("/bin/bash -lc 'ndenv versions'"), :if => os[:family] == 'darwin' do
-  its(:stdout) { should contain('v0.10.40') }
-  its(:stdout) { should contain('v0.12.4') }
-end
+else
+  describe command("/bin/bash -lc 'ndenv versions'") do
+    its(:stdout) { should contain('v0.10.40') }
+    its(:stdout) { should contain('v0.12.4') }
+  end
 
-describe command("/bin/bash -lc 'ndenv global'"), :if => os[:family] == 'darwin' do
-  its(:stdout) { should contain('v0.10.40') }
-  its(:stdout) { should_not contain('v0.12.4') }
-end
+  describe command("/bin/bash -lc 'ndenv global'") do
+    its(:stdout) { should contain('v0.10.40') }
+    its(:stdout) { should_not contain('v0.12.4') }
+  end
 
-describe command("/bin/bash -lc 'node --version'"), :if => os[:family] == 'darwin' do
-  its(:stdout) { should contain('v0.10.40') }
-  its(:stdout) { should_not contain('v0.12.4') }
+  describe command("/bin/bash -lc 'node --version'") do
+    its(:stdout) { should contain('v0.10.40') }
+    its(:stdout) { should_not contain('v0.12.4') }
+  end
 end
 
 # Check whether variables of 'FGtatsuro.anyenv' affects the behavior of this role properly.
