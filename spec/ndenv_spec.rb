f3b99247bb019752a30cd4593db5a4cc1d60f478
FGtatsuro <fujiistorage@gmail.com>
2016-12-08 19:55:24 +0900

    Login option is passed as first argument.

--- spec/ndenv_spec.rb
+++ spec/ndenv_spec.rb
@@ -1,36 +1,24 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
 if ENV['TARGET_HOST'].include?('container') then
-  describe command("su -s /bin/bash -c 'ndenv versions' - #{ENV['ANYENV_OWNER']}") do
-    its(:stdout) { should contain('v0.10.40') }
-    its(:stdout) { should contain('v0.12.4') }
-  end
-
-  describe command("su -s /bin/bash -c 'ndenv global' - #{ENV['ANYENV_OWNER']}") do
-    its(:stdout) { should contain('v0.10.40') }
-    its(:stdout) { should_not contain('v0.12.4') }
-  end
-
-  describe command("su -s /bin/bash -c 'node --version' - #{ENV['ANYENV_OWNER']}") do
-    its(:stdout) { should contain('v0.10.40') }
-    its(:stdout) { should_not contain('v0.12.4') }
-  end
-
+  command_base = "su -l -s /bin/bash -c '%s' #{ENV['ANYENV_OWNER']}"
 else
-  describe command("/bin/bash -lc 'ndenv versions'") do
-    its(:stdout) { should contain('v0.10.40') }
-    its(:stdout) { should contain('v0.12.4') }
-  end
+  command_base = "/bin/bash -lc '%s'"
+end
 
-  describe command("/bin/bash -lc 'ndenv global'") do
-    its(:stdout) { should contain('v0.10.40') }
-    its(:stdout) { should_not contain('v0.12.4') }
-  end
+describe command(command_base % 'ndenv versions') do
+  its(:stdout) { should contain('v0.10.40') }
+  its(:stdout) { should contain('v0.12.4') }
+end
 
-  describe command("/bin/bash -lc 'node --version'") do
-    its(:stdout) { should contain('v0.10.40') }
-    its(:stdout) { should_not contain('v0.12.4') }
-  end
+describe command(command_base % 'ndenv global') do
+  its(:stdout) { should contain('v0.10.40') }
+  its(:stdout) { should_not contain('v0.12.4') }
+end
+
+describe command(command_base % 'node --version') do
+  its(:stdout) { should contain('v0.10.40') }
+  its(:stdout) { should_not contain('v0.12.4') }
 end
 
 # Check whether variables of 'FGtatsuro.anyenv' affects the behavior of this role properly.
